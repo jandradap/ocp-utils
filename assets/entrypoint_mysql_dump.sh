40f670c2f48744540ebb3c97259a16238f085e25
Jorge Andrada <jandrada@viewnext.com>
2020-04-20 10:59:04 +0200

    Fix mysqldump

--- assets/entrypoint_mysql_dump.sh
+++ assets/entrypoint_mysql_dump.sh
@@ -2,7 +2,7 @@
 
 DB_USER=${DB_USER:-${MYSQL_ENV_DB_USER}}
 DB_PASS=${DB_PASS:-${MYSQL_ENV_DB_PASS}}
-DB_NAME=${DB_NAME:-${MYSQL_ENV_DB_NAME}}
+BACKUP_DATABASES=${BACKUP_DATABASES:-${MYSQL_ENV_DB_NAME}}
 DB_HOST=${DB_HOST:-${MYSQL_ENV_DB_HOST}}
 ALL_DATABASES=${ALL_DATABASES}
 IGNORE_DATABASE=${IGNORE_DATABASE}
@@ -10,32 +10,30 @@ BACKUP_STORAGE=${BACKUP_STORAGE:-/tmp}
 DATE_BACKUP=$(date +%Y%m%d)
 
 if [[ ${DB_USER} == "" ]]; then
-	echo "Missing DB_USER env variable"
-	exit 1
+  echo "Missing DB_USER env variable"
+  exit 1
 fi
+
 if [[ ${DB_PASS} == "" ]]; then
-	echo "Missing DB_PASS env variable"
-	exit 1
+  echo "Missing DB_PASS env variable"
+  exit 1
 fi
+
 if [[ ${DB_HOST} == "" ]]; then
-	echo "Missing DB_HOST env variable"
-	exit 1
+  echo "Missing DB_HOST env variable"
+  exit 1
 fi
 
 mkdir -p $BACKUP_STORAGE/$DATE_BACKUP
 
-if [[ ${ALL_DATABASES} == "" ]]; then
-  if [[ ${DB_NAME} == "" ]]; then
-    echo "Missing DB_NAME env variable"
-    exit 1
-  fi
-  mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "$@" "${DB_NAME}" > ${BACKUP_STORAGE}/$DATE_BACKUP/"${DB_NAME}".sql
+
+if [ "$ALL_DATABASES" = true ] ; then
+  mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/$DATE_BACKUP/all_databases.sql
 else
-	databases=`mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
-for db in $databases; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-        echo "Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > ${BACKUP_STORAGE}/$DATE_BACKUP/$db.sql
-    fi
-done
+  for db in $BACKUP_DATABASES; do
+      if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
+          echo "Dumping database: $db"
+          mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > ${BACKUP_STORAGE}/$DATE_BACKUP/$db.sql
+      fi
+  done
 fi

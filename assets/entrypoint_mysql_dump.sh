3611fc9c3b89858191559417be1b684e6fefc5b4
Jorge Andrada <jandrada@viewnext.com>
2020-04-20 17:14:25 +0200

    Clean

--- assets/entrypoint_mysql_dump.sh
+++ assets/entrypoint_mysql_dump.sh
@@ -35,22 +35,26 @@ fi
 echo -e "\nSystem Info:"
 df -h | grep mysqldump
 
-echo -e "\nCreating working directory ${BACKUP_STORAGE}/${DATE_BACKUP}"
-mkdir -p ${BACKUP_STORAGE}/${DATE_BACKUP}
-ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
-
-if [ "$ALL_DATABASES" = true ] ; then
-  echo -e "\nDumping all database option: \n Databases: ${BACKUP_DATABASES}"
-  mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/${DATE_BACKUP}/all_databases.sql
+if [ -d "${BACKUP_STORAGE}/${DATE_BACKUP}" ]; then
+  echo -e "\nERROR: Directory ${BACKUP_STORAGE}/${DATE_BACKUP} already exits."
 else
-  echo -e "\nDumping specific databases option:"
-  for db in $BACKUP_DATABASES; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-      echo -e "\t- Dumping database: $db"
-      mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_STORAGE}/${DATE_BACKUP}/${db}.sql
-    fi
-  done
-fi
+  echo -e "\nCreating working directory ${BACKUP_STORAGE}/${DATE_BACKUP}"
+  mkdir -p ${BACKUP_STORAGE}/${DATE_BACKUP}
+  ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
 
-echo -e "\nBackup list:"
-ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
+  if [ "$ALL_DATABASES" = true ] ; then
+    echo -e "\nDumping all database option: \n Databases: ${BACKUP_DATABASES}"
+    mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/${DATE_BACKUP}/all_databases.sql
+  else
+    echo -e "\nDumping specific databases option:"
+    for db in $BACKUP_DATABASES; do
+      if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
+        echo -e "\t- Dumping database: $db"
+        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_STORAGE}/${DATE_BACKUP}/${db}.sql
+      fi
+    done
+  fi
+
+  echo -e "\nBackup list:"
+  ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
+fi

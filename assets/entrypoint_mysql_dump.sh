656797784ed76db965ba13195d32b7c727acbaeb
Jorge Andrada <jandrada@viewnext.com>
2020-04-20 17:34:38 +0200

    Clean

--- assets/entrypoint_mysql_dump.sh
+++ assets/entrypoint_mysql_dump.sh
@@ -16,6 +16,7 @@ ALL_DATABASES=${ALL_DATABASES}
 IGNORE_DATABASE=${IGNORE_DATABASE}
 BACKUP_STORAGE=${BACKUP_STORAGE:-/tmp}
 DATE_BACKUP=$(date +%Y%m%d)
+BACKUP_PATH="${BACKUP_STORAGE}/${DATE_BACKUP}"
 
 if [[ ${DB_USER} == "" ]]; then
   echo -e "\nERROR: Missing DB_USER env variable"
@@ -35,32 +36,32 @@ fi
 echo -e "\nSystem Info:"
 df -h | grep mysqldump
 
-if [ -d "${BACKUP_STORAGE}/${DATE_BACKUP}" ]; then
-  echo -e "\nERROR: Directory ${BACKUP_STORAGE}/${DATE_BACKUP} already exits."
+if [ -d "${BACKUP_PATH}" ]; then
+  echo -e "\nERROR: Directory ${BACKUP_PATH} already exits."
 else
-  echo -e "\nCreating working directory ${BACKUP_STORAGE}/${DATE_BACKUP}"
-  mkdir -p ${BACKUP_STORAGE}/${DATE_BACKUP}
-  ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
+  echo -e "\nCreating working directory ${BACKUP_PATH}"
+  mkdir -p ${BACKUP_PATH}
+  ls -lah ${BACKUP_PATH}
 
   if [ "$ALL_DATABASES" = true ] ; then
     echo -e "\nDumping all database option: \n Databases: ${BACKUP_DATABASES}"
-    mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/${DATE_BACKUP}/all_databases.sql
+    mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_PATH}/all_databases.sql
+    sha1sum ${BACKUP_PATH}/all_databases.sql >> ${BACKUP_PATH}/checksums.txt
   else
     echo -e "\nDumping specific databases option:"
     for db in $BACKUP_DATABASES; do
       if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
         echo -e "\t- Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_STORAGE}/${DATE_BACKUP}/${db}.sql
+        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_PATH}/${db}.sql
+        sha1sum ${BACKUP_PATH}/${db} >> ${BACKUP_PATH}/checksums.txt
       fi
     done
   fi
 
   echo -e "\nBackup list:"
-  ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
+  ls -lah ${BACKUP_PATH}
 
-  echo -e "\nCreating sha1sum:"
-  cd ${BACKUP_STORAGE}/${DATE_BACKUP} ; 
-  for i in *.*; do sha1sum "$i" ; done) >> checksums.txt
-  cat checksums.txt
+  echo -e "\nSha1sum:"
+  cat ${BACKUP_PATH}/checksums.txt
 
 fi

650ffb5065106d2dccdd9fcebdc7c7f537b317b0
Jorge Andrada <jandrada@viewnext.com>
2020-04-20 13:03:16 +0200

    Logs

--- assets/entrypoint_mysql_dump.sh
+++ assets/entrypoint_mysql_dump.sh
@@ -10,30 +10,35 @@ BACKUP_STORAGE=${BACKUP_STORAGE:-/tmp}
 DATE_BACKUP=$(date +%Y%m%d)
 
 if [[ ${DB_USER} == "" ]]; then
-  echo "Missing DB_USER env variable"
+  echo "ERROR: Missing DB_USER env variable"
   exit 1
 fi
 
 if [[ ${DB_PASS} == "" ]]; then
-  echo "Missing DB_PASS env variable"
+  echo "ERROR: Missing DB_PASS env variable"
   exit 1
 fi
 
 if [[ ${DB_HOST} == "" ]]; then
-  echo "Missing DB_HOST env variable"
+  echo "ERROR: Missing DB_HOST env variable"
   exit 1
 fi
 
-mkdir -p $BACKUP_STORAGE/$DATE_BACKUP
-
+echo "Creating working directory ${BACKUP_STORAGE}/${DATE_BACKUP}"
+mkdir -p ${BACKUP_STORAGE}/${DATE_BACKUP}
+ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
 
 if [ "$ALL_DATABASES" = true ] ; then
-  mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/$DATE_BACKUP/all_databases.sql
+  echo -e "\nDumping all database option: \n Databases: ${BACKUP_DATABASES}"
+  mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --all-databases > ${BACKUP_STORAGE}/${DATE_BACKUP}/all_databases.sql
 else
   for db in $BACKUP_DATABASES; do
-      if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-          echo "Dumping database: $db"
-          mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > ${BACKUP_STORAGE}/$DATE_BACKUP/$db.sql
-      fi
+    echo -e "\nDumping specific databases option:"
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
+      echo -e "\n\ - Dumping database: $db"
+      mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_STORAGE}/${DATE_BACKUP}/${db}.sql
+    fi
   done
 fi
+
+ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}

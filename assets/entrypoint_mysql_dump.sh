312bfbfb12ba16aa7178901bb214a3a6a8de10a2
Jorge Andrada <jandrada@viewnext.com>
2020-04-20 13:54:29 +0200

    Clean

--- assets/entrypoint_mysql_dump.sh
+++ assets/entrypoint_mysql_dump.sh
@@ -38,10 +38,11 @@ else
   echo -e "\nDumping specific databases option:"
   for db in $BACKUP_DATABASES; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
-      echo -e "\n- Dumping database: $db"
+      echo -e "\t- Dumping database: $db"
       mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases ${db} > ${BACKUP_STORAGE}/${DATE_BACKUP}/${db}.sql
     fi
   done
 fi
 
+echo -e "Backup list:"
 ls -lah ${BACKUP_STORAGE}/${DATE_BACKUP}
